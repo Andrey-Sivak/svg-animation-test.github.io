88c3ed29629a767a59a21e0bc961b671f31135e8
a.sivak <riziy_bes@mail.ru>
2021-08-17 20:01:31 +0300

    init

---
+++ app/js/main.js
@@ -0,0 +1,49 @@
+'use strict';
+// import * as $ from 'jquery';
+
+const mobileWidth = 767;
+let isMobile = checkWidth();
+
+window.addEventListener('resize', () => {
+    isMobile = checkWidth();
+});
+
+setTimeout(() => {
+    if (!document.querySelector('.loader')) {
+        return;
+    }
+
+    const loader = document.querySelector('.loader');
+    if (loader.classList.contains('active')) {
+        loader.classList.remove('active');
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 300)
+    }
+}, 2500);
+
+window.addEventListener('load', function () {
+
+    (function loader() {
+        if (!document.querySelector('.loader')) {
+            return;
+        }
+
+        const loader = document.querySelector('.loader');
+
+        if (loader.classList.contains('active')) {
+            loader.classList.remove('active');
+        }
+
+        setTimeout(() => {
+            loader.parentElement.removeChild(loader);
+        }, 1500);
+
+    })();
+
+});
+
+function checkWidth() {
+    return mobileWidth > document.documentElement.clientWidth;
+}
